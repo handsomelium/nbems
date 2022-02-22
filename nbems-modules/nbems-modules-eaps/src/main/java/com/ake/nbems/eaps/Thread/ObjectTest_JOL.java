09bc8910eefddcd646e5e9f450133a70b30869bc
lium <11425@ake.com.cn>
2022-02-22 10:53:24 +0800

    提交注释

--- nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/Thread/ObjectTest_JOL.java
+++ nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/Thread/ObjectTest_JOL.java
@@ -3,12 +3,14 @@ package com.ake.nbems.eaps.Thread;
 import com.ake.nbems.eaps.domain.TestObjectByte;
 import org.openjdk.jol.info.ClassLayout;
 
+
 public class ObjectTest_JOL {
 
 
     public static void main(String[] args) {
         Object o = new Object();
 
+        // 得出Object o = new Object();占16个字节
         System.out.println(ClassLayout.parseInstance(o).toPrintable());
 
         synchronized (o){
