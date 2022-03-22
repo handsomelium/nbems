10f10434013082b252a94ba668a4d4e5caec394f
lium <11425@ake.com.cn>
2022-03-22 18:07:46 +0800

    提交网络编程

--- nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/socket/socket1/ClientDemo.java
+++ nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/socket/socket1/ClientDemo.java
@@ -2,8 +2,12 @@ package com.ake.nbems.eaps.socket.socket1;
 
 
 import java.io.*;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketAddress;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
+import java.util.zip.Deflater;
 
 public class ClientDemo {
 
@@ -25,6 +29,11 @@ public class ClientDemo {
 
             Scanner sc = new Scanner(System.in);
 
+            /*// 输入流接收服务端的回应
+            InputStream is = socket.getInputStream();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));*/
+
             while (true){
 
                 System.out.println("请说：");
@@ -34,6 +43,9 @@ public class ClientDemo {
                 // 4、发送消息
                 ps.println(msg);
 
+                // 打印服务端回应
+                //System.out.println(br.readLine());
+
                 ps.flush();
 
             }
@@ -41,10 +53,30 @@ public class ClientDemo {
             // 关闭资源。
             // socket.close();
 
+            /*String strTemp = "abcdefg";
+
+            OutputStream os = socket.getOutputStream();
+
+            byte[] a = strTemp.getBytes(StandardCharsets.UTF_8);
+            int length = a.length;
+            byte[] b = new byte[4];
+            b[0] = (byte) (length >> 24);
+            b[1] = (byte) ((length >> 16) & 0xFF);
+            b[2] = (byte) ((length >> 8) & 0xFF);
+            b[3] = (byte) (length & 0xFF);
+            os.write(b);
+            os.write(a);
+
+            os.close();*/
+
+
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
 
     }
+
+
 }
--- nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/socket/socket1/ServerDemo.java
+++ nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/socket/socket1/ServerDemo.java
@@ -3,10 +3,15 @@ package com.ake.nbems.eaps.socket.socket1;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
-public class ServerDemo {
+public class ServerDemo extends ClientDemo {
 
     public static void main(String[] args) {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(3);
 
         System.out.println("===服务端启动成功===");
 
@@ -23,7 +28,7 @@ public class ServerDemo {
                 InputStream inputStream = socket.getInputStream();
 
                 // 4、把字节输入流包装成缓冲字符输入流进行消息的接收
-                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream));
+                BufferedReader br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 
                 // 5、按照行读取消息
                 String msg;
@@ -32,7 +37,20 @@ public class ServerDemo {
 
                     System.out.println(socket.getRemoteSocketAddress() + "说了：" + msg);
 
+                    /*OutputStream os = socket.getOutputStream();
+                    PrintStream ps = new PrintStream(os);
+
+                    ps.println("response is ok !!");*/
+
                 }
+
+                /*byte[] b = new byte[1024];
+
+                int read = inputStream.read(b);
+
+                if (read != -1){
+                    System.out.println(new String(b, 0, read));
+                }*/
             }
 
 
