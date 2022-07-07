c33880945a5389704547bd011457dc6c70dcc6e8
lium <11425@ake.com.cn>
2022-07-07 17:05:16 +0800

    提交redis工具类

--- nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/test/TestRedis.java
+++ nbems-modules/nbems-modules-eaps/src/main/java/com/ake/nbems/eaps/test/TestRedis.java
@@ -1,12 +1,15 @@
 package com.ake.nbems.eaps.test;
 
 import com.ake.nbems.eaps.NbemsEapsApplication;
+import com.ake.nbems.eaps.utils.RedisUtil;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -19,18 +22,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class TestRedis {
 
     @Autowired
-    public RedisTemplate redisTemplate;
+    public RedisUtil redisUtil;
 
 
     @Test
     public void setRedis(){
-        redisTemplate.opsForValue().set("name", "lium");
-        redisTemplate.opsForValue().set("age", 25);
+        redisUtil.set("name", "张三");
+        redisUtil.set("age", 18);
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "张三");
+        map.put("age", 18);
+        map.put("sex", "男");
+        map.put("score", 90);
+        redisUtil.hmset("student", map);
     }
 
     @Test
     public void getRedis(){
-        Object name = redisTemplate.opsForValue().get("name");
+        Object name = redisUtil.get("name");
         System.out.println(name);
     }
 
